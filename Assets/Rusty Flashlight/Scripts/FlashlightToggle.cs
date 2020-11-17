6d1a2f6a53eb30ef8062cef36bcc5ee761803940
JBengey <jack.bengeyfield@gmail.com>
2020-11-17 21:58:33 +0000

    Shortned tunnel, added material to tunnel, added basic lights and XR rig

---
+++ Assets/Rusty Flashlight/Scripts/FlashlightToggle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class FlashlightToggle : MonoBehaviour
+{
+    public GameObject lightGO; //light gameObject to work with
+    private bool isOn = false; //is flashlight on or off?
+
+    // Use this for initialization
+    void Start()
+    {
+        //set default off
+        lightGO.SetActive(isOn);
+    }
+
+    //toggle flashlight
+    public void togglelight()
+    {
+          //toggle light
+          isOn = !isOn;
+          //turn light on
+          if (isOn)
+          {
+          lightGO.SetActive(true);
+          }
+          //turn light off
+          else
+          {
+          lightGO.SetActive(false);
+
+      }
+    }
+}
